965950025b9a28034d38acc11e6d7de1736711d0
Robert Spencer <rems.byustudies@gmail.com>
2019-03-02 09:09:31 -0700

    initial commit

---
+++ JavaScript/NewTestFile.js
@@ -0,0 +1,16 @@
+// Test File for gitHub
+// FUNCTION DECLARATION vs FUNCTION EXPRESSION
+
+// FUNCTION EXPRESSION
+/* 
+var func1 = function (){
+
+}
+*/
+
+// FUNCTION DECLARATION
+/* 
+function myFunc (){
+
+}
+*/
